09933d9dece7a5ee13ea161ebb6dab1037e12363
Pino223 <riveluis223@gmail.com>
2024-12-09 11:07:45 -0700

    examen

---
+++ Examen recuperacion/Examen recuperacion/ModeloVista/VMcalculadora.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Examen_recuperacion.ModeloVista
+{
+    public class VMcalculadora : BaseViewModel
+    {
+        #region VARIABLES
+        int _Latidos15seg;
+        int _FrecuenciaCardiaca;
+        string _Categoriadelafrecuencia;
+
+        #endregion
+        #region CONSTRUCTOR
+        public VMcalculadora(INavigation navigation)
+        {
+            Navigation = navigation;
+
+        }
+        #endregion
+        #region OBJETOS
+        public int Latidos15seg
+        {
+            get { return _Latidos15seg; }
+            set { SetValue(ref _Latidos15seg, value); }
+        }
+
+        public int FrecuenciaCardiaca
+        {
+            get { return _FrecuenciaCardiaca; }
+            set { SetValue(ref _FrecuenciaCardiaca, value); }
+        }
+
+        public string CategoriaFrecuenciaCardiaca
+        {
+            get { return _Categoriadelafrecuencia; }
+            set { SetValue(ref _Categoriadelafrecuencia, value); }
+        }
+        #endregion
+        #region PROCESOS
+
+        public void CalcularFrecuencia()
+        {
+            FrecuenciaCardiaca = Latidos15seg * 4;
+
+            if (FrecuenciaCardiaca < 60)
+                CategoriaFrecuenciaCardiaca = "Frecuencia cardíaca baja";
+            else if (FrecuenciaCardiaca > 100)
+                CategoriaFrecuenciaCardiaca = "Frecuencia cardíaca alta";
+            else
+                CategoriaFrecuenciaCardiaca = "Frecuencia cardíaca normal";
+        }
+        #endregion
+        #region COMANDOS
+        public ICommand CalcularFrecuenciaCommand => new Command(CalcularFrecuencia);
+        #endregion
+    }
+}
---
+++ Examen recuperacion/Examen recuperacion/Vista/LatidosDetectados.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Examen_recuperacion.ModeloVista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen_recuperacion.Vista
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LatidosDetectados : ContentPage
+    {
+        public LatidosDetectados()
+        {
+            InitializeComponent();
+            BindingContext = new VMcalculadora(Navigation);
+        }
+    }
+}
